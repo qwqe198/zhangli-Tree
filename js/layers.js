ce1fb8505e4d8e4b2c56a44cd74c816f9376271b
qwqe198 <146399264+qwqe198@users.noreply.github.com>
2025-08-22 18:06:50 +0800

    Add files via upload

---
+++ js/layers.js
@@ -0,0 +1,138 @@
+addLayer("p", {
+    name: "张力点", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() {
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#00FF00",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "张力点", // Name of prestige currency
+    baseResource: "点数", // Name of resource prestige is based on
+    baseAmount() { return player.points }, // Get the current amount of baseResource
+    type: "normal", // normal:膨胀资源层 static: 非膨胀资源层 使用时要加双引号
+    exponent: 0.1, // Prestige currency exponent 初始值0.5
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+if(hasUpgrade("p",22))mult=mult.mul(3.3333)
+if(hasUpgrade("p",23))mult=mult.div(1.1111)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+exp = new Decimal(1)
+if(hasUpgrade("p",22))exp=exp.sub(0.25)
+if(hasUpgrade("p",23))exp=exp.add(0.333)
+
+        return exp
+    },
+    eff() {
+        let eff = player.p.points.add(1).log10().mul(0.1).add(1)
+eff=eff.pow(buyableEffect("p",12))
+        return eff
+    },
+    effectDescription() { return `基础点数获取${format(layers.p.eff())}` },
+    upgrades: {
+        11: {
+            description: "1.极致的指数 点数获取变成原来的平方",
+            cost() { return new Decimal(1) },
+            unlocked() { return true },
+        },
+        12: {
+            description: "点数获取变成原来的平方",
+            cost() { return new Decimal(2) },
+            unlocked() { return true },
+        },
+        13: {
+            description: "点数获取变成原来的平方",
+            cost() { return new Decimal(3) },
+            unlocked() { return true },
+        },
+        14: {
+            description: "点数获取变成原来的平方",
+            cost() { return new Decimal(5) },
+            unlocked() { return true },
+        },
+        15: {
+            description: "点数获取变成原来的平方",
+            cost() { return new Decimal(8) },
+            unlocked() { return true },
+        },
+21: {
+            description: "解锁一个可购买",
+            cost() { return new Decimal(13) },
+            unlocked() { return true },
+        },
+22: {
+            description: "3.超大乘数 张力点获取x3.3333，但是指数-0.025",
+            cost() { return new Decimal(144) },
+            unlocked() { return true },
+        },
+23: {
+            description: "4.超大指数 张力点获取指数+0.0333，但是获取/1.1111",
+            cost() { return new Decimal(377) },
+            unlocked() { return true },
+        },
+24: {
+            description: "解锁第二个可购买",
+            cost() { return new Decimal(2584) },
+            unlocked() { return true },
+        },
+25: {
+            description: "当前残局",
+            cost() { return new Decimal(28657) },
+            unlocked() { return true },
+        },
+    },
+buyables: {
+        11: {
+            cost(x = getBuyableAmount(this.layer, this.id)) {
+                var c = new Decimal(1.618).pow(x.add(6)).floor()
+                return c
+ },
+            display() { return `点数获取<br />x${format(buyableEffect(this.layer, this.id), 2)}. (下一个: ${format(this.effect(getBuyableAmount(this.layer, this.id).add(1)))}).花费: ${format(this.cost(getBuyableAmount(this.layer, this.id)))}张力点<br>等级: ${format(getBuyableAmount(this.layer, this.id))}` },
+            canAfford() { return player.p.points.gte(this.cost()) },
+            buy() {
+                player.p.points = player.p.points.sub(this.cost())
+                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+            },
+            title() {
+                return "2.巨大张力的效果"
+            },
+            effect(x = getBuyableAmount(this.layer, this.id)) {
+                var eff = new Decimal(10).pow(x)
+                return eff
+            },
+            unlocked() { return hasUpgrade("p",21) },
+        },
+ 12: {
+            cost(x = getBuyableAmount(this.layer, this.id)) {
+                var c = new Decimal(1.618).pow(x.pow(2).add(16)).floor()
+                return c
+ },
+            display() { return `张力点效果<br />^${format(buyableEffect(this.layer, this.id), 2)}. (下一个: ${format(this.effect(getBuyableAmount(this.layer, this.id).add(1)))}).花费: ${format(this.cost(getBuyableAmount(this.layer, this.id)))}张力点<br>等级: ${format(getBuyableAmount(this.layer, this.id))}` },
+            canAfford() { return player.p.points.gte(this.cost()) },
+            buy() {
+                player.p.points = player.p.points.sub(this.cost())
+                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+            },
+            title() {
+                return "指数加成"
+            },
+            effect(x = getBuyableAmount(this.layer, this.id)) {
+                var eff = x.mul(0.1).add(1)
+                return eff
+            },
+            unlocked() { return hasUpgrade("p",24) },
+        },
+        },
+    
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        { key: "p", description: "p: 进行声望重置", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    layerShown() { return true }
+})
+
